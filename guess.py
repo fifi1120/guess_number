64f689be539349de0e61abea03c16bc54262ffb6
Fiona Xiong <xinyiwanshi1996@163.com>
2022-09-05 19:28:28 +0800

    user_designed

--- guess.py
+++ guess.py
@@ -1,5 +1,9 @@
 import random
-number = random.randint(1,100)
+start = input ("请决定随机数开始值：")
+start = int (start)
+end = input ("请决定随机数末尾值：")
+end = int (end)
+number = random.randint(start , end)
 i = 0
 while True:
     user = input ("请猜一个数字：")
@@ -7,11 +11,10 @@ while True:
     user = int (user)
     if user > number:
         print ("你猜的数字比答案大哦！")
-        print ("这是你猜的第",i,"次哦")
     elif user < number:
         print ("你猜的数字比答案小哦！")
-        print ("这是你猜的第",i,"次哦")
     else:
         print ("终于猜对了！")
+        print ("这是你猜的第",i,"次哦")
         break
-print ("这是你猜的第",i,"次哦")
+    print ("这是你猜的第",i,"次哦")
